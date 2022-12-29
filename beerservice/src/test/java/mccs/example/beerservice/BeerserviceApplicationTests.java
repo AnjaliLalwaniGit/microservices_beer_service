0be81180bdf4489cf1bc9f4dc697ce43355aea87
Anjali Lalwani <anjali.s.lalwani@gmail.com>
2022-12-29 13:01:13 -0800

    beer service microservices example
    Initial commit

---
+++ beerservice/src/test/java/mccs/example/beerservice/BeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package mccs.example.beerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

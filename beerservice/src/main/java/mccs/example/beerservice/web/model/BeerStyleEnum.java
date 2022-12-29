4d8080ad903114b783c427f2f76698df94e9267c
Anjali Lalwani <anjali.s.lalwani@gmail.com>
2022-12-29 14:07:20 -0800

    adding web data model
    adding web data model

---
+++ beerservice/src/main/java/mccs/example/beerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package mccs.example.beerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
